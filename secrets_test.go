d476719486dcf2cbfbedf2b801fd425033ba9764
Jon Olsson <jon.olsson@gmail.com>
2024-11-24 16:26:54 +0100

    Zero dependencies

--- secrets_test.go
+++ secrets_test.go
@@ -5,81 +5,149 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"slices"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestSecret(t *testing.T) {
-	assert := assert.New(t)
+func assertEqual[V comparable](t *testing.T, expected, actual V) {
+	t.Helper()
 
+	if expected != actual {
+		t.Errorf("expected `%v` got `%v`", expected, actual)
+	}
+}
+
+func assertError(t *testing.T, err error) {
+	t.Helper()
+
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+}
+
+func assertNil[V any](t *testing.T, v *V) {
+	t.Helper()
+
+	if v != nil {
+		t.Errorf("expected nil, got %v", v)
+	}
+}
+
+func assertNotNil[V any](t *testing.T, v *V) {
+	t.Helper()
+
+	if v == nil {
+		t.Errorf("expected not nil, got %v", v)
+	}
+}
+
+func toStrSlice(s []*Secret) []string {
+	res := []string{}
+
+	for _, secret := range s {
+		res = append(res, secret.value)
+	}
+
+	return res
+}
+
+func TestSecret(t *testing.T) {
 	t.Run("Basic", func(t *testing.T) {
 		s := Secret{value: "foo"}
-		assert.Equal("**REDACTED**", fmt.Sprintf("%v", s))
-		assert.Equal("**REDACTED**", s.String())
-		assert.Equal("foo", s.ExposeSecret())
+
+		assertEqual(t, "**REDACTED**", fmt.Sprintf("%v", s))
+		assertEqual(t, "**REDACTED**", s.String())
+		assertEqual(t, "foo", s.ExposeSecret())
 	})
 
 	t.Run("Mask", func(t *testing.T) {
 		s1 := &Secret{value: "foo"}
 		s2 := &Secret{value: "bar"}
 
-		assert.Equal("this is a random **REDACTED** string that **REDACTED** contains the exposed secret",
+		assertEqual(t, "this is a random **REDACTED** string that **REDACTED** contains the exposed secret",
 			Mask("this is a random foo string that foo contains the exposed secret", s1))
 
-		assert.Equal("this is a random foo string that foo contains the exposed secret",
+		assertEqual(t, "this is a random foo string that foo contains the exposed secret",
 			Mask("this is a random foo string that foo contains the exposed secret", s2))
 
-		assert.Equal("this is a random **REDACTED** string that **REDACTED** contains the exposed secret",
+		assertEqual(t, "this is a random **REDACTED** string that **REDACTED** contains the exposed secret",
 			Mask("this is a random foo string that foo contains the exposed secret", s1, s2))
 
-		assert.Equal(
-			errors.New("some error **REDACTED**"),
-			MaskErr(errors.New("some error foo"), s1))
+		assertEqual(t,
+			errors.New("some error **REDACTED**").Error(),
+			MaskErr(errors.New("some error foo"), s1).Error())
 	})
 
 	t.Run("Split", func(t *testing.T) {
-		assert.Equal([]*Secret{New("foo")}, New("foo").Split(" "))
-		assert.Equal([]*Secret{New("foo"), New("bar")}, New("foo bar").Split(" "))
+		tests := []struct {
+			input    string
+			expected []string
+		}{
+			{
+				input:    "foo",
+				expected: []string{"foo"},
+			},
+			{
+				input:    "foo bar",
+				expected: []string{"foo", "bar"},
+			},
+		}
+
+		for _, test := range tests {
+			splitSecret := New(test.input).Split(" ")
+			actual := toStrSlice(splitSecret)
+
+			if !slices.Equal(test.expected, actual) {
+				t.Errorf("expected %v, got %v", test.expected, actual)
+			}
+		}
 	})
 
 	t.Run("FromEnv", func(t *testing.T) {
 		secret, err := FromEnv("DOES_NOT_EXIST_FOR_SURE")
-		assert.Error(err)
-		assert.Nil(secret)
+		assertError(t, err)
+		assertNil(t, secret)
 
-		assert.NoError(os.Setenv("THIS_WILL_EXIST", "foo"))
+		assertNoError(t, os.Setenv("THIS_WILL_EXIST", "foo"))
 		secret, err = FromEnv("THIS_WILL_EXIST")
-		assert.NoError(err)
-		assert.NotNil(secret)
-		assert.Equal(secret.ExposeSecret(), "foo")
+		assertNoError(t, err)
+		assertNotNil(t, secret)
+		assertEqual(t, secret.ExposeSecret(), "foo")
 
 		// Cleanup:
-		assert.NoError(os.Unsetenv("THIS_WILL_EXIST"))
+		assertNoError(t, os.Unsetenv("THIS_WILL_EXIST"))
 	})
 
 	t.Run("FromFile", func(t *testing.T) {
 		secrets, err := os.MkdirTemp("/tmp", "utest-secrets")
-		assert.NoError(err)
+		assertNoError(t, err)
 
 		defer os.RemoveAll(secrets)
 
 		secret, err := FromFile("HEMLIZ")
-		assert.Error(err)
-		assert.Nil(secret)
+		assertError(t, err)
+		assertNil(t, secret)
 
 		path := path.Join(secrets, "HEMLIZ")
 		os.WriteFile(path, []byte("LOL!"), 0o644)
 		secret, err = FromFile(path)
-		assert.Error(err)
-		assert.Nil(secret)
+		assertError(t, err)
+		assertNil(t, secret)
 
-		assert.NoError(os.Remove(path))
+		assertNoError(t, os.Remove(path))
 
 		os.WriteFile(path, []byte("OMG!"), 0o600)
 		secret, err = FromFile(path)
-		assert.NoError(err)
-		assert.NotNil(secret)
-		assert.Equal("OMG!", secret.ExposeSecret())
+		assertNoError(t, err)
+		assertNotNil(t, secret)
+		assertEqual(t, "OMG!", secret.ExposeSecret())
 	})
 }
